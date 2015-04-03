8ad072b1ab956b4a37117f6ca1d96a5b786fda94
Artur Bialkowski <Kabotyn@users.noreply.github.com>
2015-04-03 23:59:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,12 @@
 
 int main (int argc, char * const argv[]) {
     
-//    Alphabet2Morse a2m;
-//	a2m.setText("DuPa BlAdA");
-//	std::cout << "Normal text: " << a2m.getText() << std::endl;
-//	std::cout << "Morse text: ";
-//	a2m.translate2Morse();
-//	a2m.showMorse();
-//	std::cout << std::endl;
-	
-	std::ofstream stream("./../../test.wav", std::ios::binary);
-	stream.write("RIFF", 4);
-	
+    Alphabet2Morse a2m;
+	std::cout << "Normal text: " << a2m.getText() << std::endl;
+	std::cout << "Morse text: ";
+	a2m.translate2Morse();
+	a2m.showMorse();
+	std::cout << std::endl;
+
     return 0;
 }
